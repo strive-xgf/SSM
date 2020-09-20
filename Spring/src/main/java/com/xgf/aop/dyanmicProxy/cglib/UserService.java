b3148df3002b6b3bd27d2650d329ef6a5d23495b
strive_gf@163.com <strive_gf@163.com>
2020-09-20 10:02:44 +0800

    测试CGLIB动态代理实现Spring AOP(cglib)

---
+++ Spring/src/main/java/com/xgf/aop/dyanmicProxy/cglib/UserService.java
@@ -0,0 +1,10 @@
+package com.xgf.aop.dyanmicProxy.cglib;
+
+public class UserService {
+    public void save(){
+        System.out.println("save service ......");
+    }
+    public void delete(){
+        System.out.println("delete service ......");
+    }
+}
--- Spring/src/main/java/com/xgf/aop/dyanmicProxy/jdk/UserDao.java
+++ Spring/src/main/java/com/xgf/aop/dyanmicProxy/jdk/UserDao.java
@@ -4,7 +4,6 @@ package com.xgf.aop.dyanmicProxy.jdk;
 * 被代理类
 * */
 public interface UserDao {
-
     public void save(String name);  //保存
     public void delete();           //删除
 }
